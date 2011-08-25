e51c40148242a2b1b7ac5c72e54a67a9b14c7a75
csl <vic@GPL.amit.com.tw>
2011-08-25 16:48:39 +0800

    CHG: branch for customrer

--- simple.cpp
+++ simple.cpp
@@ -1,261 +1,157 @@
-#include<iostream>
-#include<vector>
+#include<cmath>
+#include<stdlib.h>
 #include<fstream>
 #include<cmath>
 #include<string>
-#include<cstring>
+#include<iostream>
 #include<climits>
-//#include<stdio.h>
+#include<cmath>
 #include<stdlib.h>
+#include <list>
+#include <map>
+#include <queue>
+#include <vector>
 
-//#define DOUBLE_MAX  99999999999
-//#define DEBUG
+#define PI  4.0*atan(1.0)
+
+#define DEBUG
 
 using namespace std;
 
-class Node;
-class Edge;
-class Path;
-
-const double PI = 4.0*atan(1.0);
-
-vector<Node*> nodes;
-vector<Edge*> edges;
-vector<Path*> paths;
+class Edge; 
 
 class Node
 {
 public:
-	Node(char* mid)
-	{
-		strcpy(id, mid);
-		previous = NULL;
-		dStart = INT_MAX;
-		nodes.push_back(this);
-		lat=0;
-		lng=0;
-	}
+	string    name;
+	bool      visited;
+	double       cost;
+	double		 lat;
+	double		 lng;
+	class Node      *back_pointer;
+	list<class Edge*> neighbors;
 
-	void setLat(double mlat)
-	{
-		lat = mlat;
+	Node(string const &n)
+	{ 
+		name = n;
+		visited = false;
+		cost = 0.0;
+		lat=0.0;
+		lng=0.0;
 	}
+};
 
-	void setLng(double mlng)
-	{
-		lng = mlng;
-	}
+class Tracker
+{
+public:
+	  vector<string> id;
+	  double cost;
+};
 
-	double getLat()
-	{
-		return lat;
-	}
+class Edge 
+{
+public:
+	double weight;
+	class Node * dest;
 
-	double getLng()
-	{
-		return lng;
-	}
+	Edge(double c,Node *d = NULL):weight(c),dest(d){}
+
+	~Edge(){if(dest)delete dest;}
+};
+
+class NodeMap{
+public:
+	Tracker track;	
+	map<string,Node*> nodemap;
 	
-public:
-	char id[255];
-	class Node* previous;
-	double lat;
-	double lng;
-	double dStart;
-};
-
-class Edge
-{
-public:
-	Edge(Node* node1, Node* node2, double distance)
-		: node1(node1), node2(node2), distance(distance)
+	Node* find_in_nodemap(string const &name)
 	{
-		edges.push_back(this);
+		Node* result = nodemap[name];
+		if ( result == 0 )
+			result = nodemap[name] = new Node(name);
+		return result;
 	}
-
-	bool Connects(Node* node1, Node* node2)
+	#if 0
+	void setTracker(string src, string dst)
 	{
-		return ((node1 == this->node1 && node2 == this->node2));
-	}
-public:
-	Node* node1;
-	Node* node2;
-	double distance;
-};
-
-class Path
-{
-public:
-	  Path(double th)
-	  {
-		 total = th;
-		 //path.push_back(this);
-	  }
-
-	  void setP(string str)
-	  {
-			mpath.push_back(str);
-	  }
-
-	  void pushb()
-	  {
-		 paths.push_back(this);
-	  }
-
-	  void print()
-	  {
-			cout << total << " ";
-			if (total != -1)
-			{
-				for (int i=mpath.size()-1; i>=0; i--)
-				{
-					cout << mpath[i] << " ";
-				}
-			}
-			cout << endl;
-      }
-
-private:
-	 double total;
-	 vector<string> mpath;
-};
-
-
-Node* Et(vector<Node*>& nodes)
-{
-	int size = nodes.size();
-	if (size == 0) return NULL;
-
-	int smallestPosition = 0;
-	Node* smallest = nodes.at(0);
-	for (int i=1; i<size; ++i)
-	{
-		Node* current = nodes.at(i);
-		if (current->dStart < smallest->dStart)
-		{
-			smallest = current;
-			smallestPosition = i;
-		}
-	}
-	nodes.erase(nodes.begin() + smallestPosition);
-	return smallest;
-}
-
-bool Contains(vector<Node*>& nodes, class Node* node)
-{
-	const int size = nodes.size();
-	for(int i=0; i<size; ++i)
-	{
-		if (node == nodes.at(i))
-		{
-			return true;
-		}
-	}
-	return false;
-}
-
-vector<Node*>* AdjacentNodes(Node* node)
-{
-	vector<Node*>* adjacentNodes = new vector<Node*>();
-	const int size = edges.size();
-	for(int i=0; i<size; ++i)
-	{
-		Edge* edge = edges.at(i);
-		Node* adjacent = NULL;
-		if (edge->node1 == node)
-		{
-			adjacent = edge->node2;
-		}
-		else if (edge->node2 == node)
-		{
-			adjacent = edge->node1;
-		}
-		if (adjacent && Contains(nodes, adjacent))
-		{
-			adjacentNodes->push_back(adjacent);
-		}
-	}
-	return adjacentNodes;
-}
-
-void PrintRoute(Node* dest)
-{
-	Node* p = dest;
-	Path* path = NULL;
-
-	if (dest == NULL)
-	{
-		return;
-	}
-	else
-	{
-		path = new Path(dest->dStart);
-	}
-
-	if (dest->dStart != -1)
-	{
-		while (p)
-		{
-		//cout << previous->id << " ";
-			path->setP(p->id);
-			p = p->previous;
-		}
-	}
-	//cout << endl;
-	path->pushb();
-}
-
-double Distance(Node* node1, Node* node2)
-{
-	const int size = edges.size();
-	for(int i=0; i<size; ++i)
-	{
-		Edge* edge = edges.at(i);
-		if (edge->Connects(node1, node2))
-		{
-			return edge->distance;
-		}
-	}
-	return -1; // should never happen
-}
-
-void Das()
-{
-	while (nodes.size() > 0)
-	{
-		Node* smallest = Et(nodes);
-		vector<Node*>* adjacentNodes =
-			AdjacentNodes(smallest);
-
-		const int size = adjacentNodes->size();
-		for (int i=0; i<size; ++i)
-		{
-			Node* adjacent = adjacentNodes->at(i);
-			double distance = Distance(smallest, adjacent) +
-				smallest->dStart;
-			if (distance < adjacent->dStart)
-			{
-				adjacent->dStart = distance;
-				adjacent->previous = smallest;
-			}
-		}
-		delete adjacentNodes;
-	}
-}
-
-class Node* findnode(char *p)
-{
-    const int size = nodes.size();
-    for(int i=0; i<size; ++i)
-    {	
-        if (!strcmp(p, nodes.at(i)->id))
+		class Tracker *mytracker = new Trakcer();
+        map<string,Node*>::iterator im;
+        for(im = nm.nodemap.begin(); im != nm.nodemap.end(); im++)
         {
-		 	//cout << "match: " << nodes.at(i)->id << " = " << p << endl;
-			return nodes.at(i);
-        }
-    }
+			if (src.compare((*im).second->name)) continue;
+			mytracker->id.push_back(src.compare((*im).second->name))
 
-	return NULL;
+            list<Edge*> neighbors = (*im).second->neighbors;
+            list<Edge*>::iterator e;
+            for(e = neighbors.begin(); e != neighbors.end(); e++)
+			{
+                cout << "   -> " << (*e)->dest->name << " weight " << (*e)->weight <<endl;
+            }
+
+        }
+	}
+	#endif
+
+	friend ostream& operator<<(ostream& o, NodeMap nm){
+		map<string,Node*>::iterator im;
+		for(im = nm.nodemap.begin(); im != nm.nodemap.end(); im++)
+		{
+			o << (*im).second->name << endl;
+	        list<Edge*> neighbors = (*im).second->neighbors;
+			list<Edge*>::iterator e;
+			for(e = neighbors.begin(); e != neighbors.end(); e++){
+				cout << "   -> " << (*e)->dest->name << " weight " << (*e)->weight <<endl;
+			}
+
+		}
+		return o;
+	}
+};
+
+struct compare
+{  
+   bool operator()(Node * &a,Node * &b)const{
+      return b->cost < a->cost;      
+   }
+};
+
+void reset_nodes(NodeMap &nm)
+{
+	map<string,Node*>::iterator im;
+	for(im = nm.nodemap.begin(); im != nm.nodemap.end(); im++){
+		(*im).second->cost = 0; 
+		(*im).second->back_pointer = 0;
+		(*im).second->visited = false;
+	}
+}
+
+void dijkstra(string s,string t,NodeMap &nodes){
+
+	Node* source = nodes.nodemap[s];
+	Node* target = nodes.nodemap[t];
+
+	reset_nodes(nodes);
+
+	priority_queue<Node *, deque<Node*>, compare>pq;
+	pq.push(source);
+	while(!pq.empty()){
+
+		Node* curr = pq.top(); 
+		pq.pop();
+		curr->visited = true;
+
+		list<Edge*>::iterator edge;
+		for(edge = curr->neighbors.begin(); edge != curr->neighbors.end(); edge++){
+			Node *ne = (*edge)->dest;
+			if(!ne->visited){
+				ne->cost += (*edge)->weight + curr->cost;
+				ne->visited = true;
+				cout << " pushing " << ne->name << " cost " << ne->cost << endl;
+				ne->back_pointer = curr;
+				pq.push(ne);
+			}
+		}
+	}
 }
 
 double ConvertDegreeToRadians(double degrees)
@@ -275,207 +171,208 @@ double GetDistance(double Lat1, double Long1, double Lat2, double Long2)
 	return d;
 }
 
-
-string LTrim(const string& str)
-{
-	return str.substr(str.find_first_not_of(" \n\r\t"));
-}
-
-string RTrim(const string& str)
-{
-	return str.substr(0,str.find_last_not_of(" \n\r\t")+1);
-}
-
 string trim(const string& str)
 {
 	if (!str.compare("")) return str;
-	return LTrim(RTrim(str));
-} 
+	string s1 = str.substr(0,str.find_last_not_of(" \n\r\t")+1);
+	string s2 = s1.substr(s1.find_first_not_of(" \n\r\t"));
+
+	return s2;
+	
+}
+ 
 int main(int argc, char **argv)
 {
-	string sp;
-	char file_path[255]="";
-	class Node* a = NULL;
-    	class Node* b = NULL;
-	int weight=0;
-	char * pch;
-	char buffer[255]="";
-	char data[255]="";
-	int count=0;
-	int loop=0;
-	char dstart[255]="",dend[255]="";
+	char filename[30];
+	string datain;
+	string fsrc, fdst;
+	class NodeMap node_map;
+	class Node *mnode;
+	class Node *src, *dst;
+	class Edge *connector;
+	//Source->neighbors.push_back(connector);
+	char cdata[33]="", mdata[33]="";
+	int mydata=0;
+	int r=0, link=0;
 
-    	cin >> file_path;
+   	cin >> filename;
+	if (!strcmp(filename,"-1")) exit(1);
 
 	while (1)
 	{
-		nodes.clear();
-		edges.clear();
-		//FILE *file = fopen(file_path, "r");
-		if (!strcmp(file_path,"-1")) break;
-		
-		ifstream fin(file_path);
-		//FILE *file = fopen(file_path, "r");
+		ifstream openfile(filename);
 
-		if(!fin)
+		if(!openfile)
 		{    
-			cout << file_path << "file error" << endl;   
-			break;
+			cout << "can't get file" << endl;   
+			exit(1);
 		}
 
-	    while (!fin.eof())
+	    while (!openfile.eof())
 	    {
-			getline(fin, sp);
-			strcpy( buffer, sp.c_str() );
-			sp = trim(sp);
-			if (!sp.compare("")) 
-					break;
-			else if (!sp.compare("<geometry>"))
+			getline(openfile, datain);
+			strcpy( cdata, datain.c_str() );
+			datain = trim(datain);
+			//geometry tag
+			if (datain.compare("<geometry>"))
 			{
-				//handler: loaction
-				while (1)
-				{
-					getline(fin, sp);
-					sp = trim(sp);
+                char *dp = strtok (cdata," ");
+				int dcounter=0;
 				
-					if (!sp.compare("<location>"))
-					{
-						//printf("location handling...\n");
-						//fetch id
-						getline(fin, sp);
-						sp = trim(sp);
-						strcpy( buffer, sp.c_str() );
-
-						int ct=0;
-						for (int j=4; buffer[j] != '<' ;j++,ct++)
-							data[ct] = buffer[j];
-						data[ct]='\0';
-
-						a = findnode(data);
-						if (a == NULL)  a = new Node(data);
-						
-						//fetch lat
-						getline(fin, sp);
-						sp = trim(sp);
-						strcpy( buffer, sp.c_str() );
-						ct=0;
-						for (int j=5; buffer[j] != '<' ;j++,ct++)
-							data[ct] = buffer[j];
-						data[ct]='\0';
-
-						a->setLat(atof(data));
-
-						//fetch lng
-						getline(fin, sp);
-						sp = trim(sp);
-						strcpy( buffer, sp.c_str() );
-						ct=0;
-						for (int j=5; buffer[j] != '<' ;j++,ct++)
-							data[ct] = buffer[j];
-						data[ct]='\0';
-						a->setLng(atof(data));
-						
-						continue;
+                while (dp != NULL)
+                {
+                    if  (dcounter == 0)
+                    {
+						string from(dp);
+						src = node_map.find_in_nodemap(from);
 					}
-					else if (!sp.compare("</location>"))
+					else if (dcounter == 1)
 					{
-						continue;
+						string to(dp);
+						dst = node_map.find_in_nodemap(to);
+
 					}
-
-					break;
-				}
-				
-				//handler: link
-				while (1)
-				{
-					strcpy( buffer, sp.c_str() );
-					int ct=0;
-					int j=0;
-
-					for (j=0; buffer[j] != '>' ;j++) ;
-					j++;
-					for (; buffer[j] != '<' ;j++)
+					else if (dcounter == 2)
 					{
-						if (buffer[j] == ' ')
-						{
-							data[ct]='\0';
-					        	a = findnode(data);
-							ct=0;
-							continue;
-						}
-
-						data[ct] = buffer[j];
-						ct++;
-					}
-					data[ct]='\0';
-				       b = findnode(data);
-					#ifdef DEBUG
-						cout << a->id << " -> " << b->id << endl;
-						cout << a->getLat() << "," << a->getLng() << endl;
-						cout << b->getLat() << "," << b->getLng() << endl;
-					#endif
-
-					double weight = GetDistance(a->getLat(), a->getLng(), 
-							b->getLat(), b->getLng());
-					
-					#ifdef DEBUG
-						cout << weight << endl;
-					#endif
-
-					Edge* e1 = new Edge(a, b, weight);
-
-					getline(fin, sp);
-					if (!sp.compare("</geometry>")) break;
-				}
+                            int weight = atoi(dp);
+							printf("weight = %d\n",weight);
+							connector = new Edge(weight, dst);
+							src->neighbors.push_back(connector);
+                    }
+                    dp = strtok (NULL, " ");
+					dcounter++;
+            	}          
 			}
 			else
 			{
-	  			pch = strtok (buffer," ");			
-
-				while (pch != NULL)
+				while (1)
 				{
-			    	switch (count)
-		        	{
-						case 0:
-							a = findnode(pch);
-							if (a == NULL)  a = new Node(pch);
-			    				count++;
-							break;
-						case 1:
-							b = findnode(pch);
-							if (b == NULL)  b = new Node(pch);
-			    				count++;
-							break;
-						case 2:
-							weight= atoi(pch);
-							//printf("%s -> %s, w=%d\n", a->id, b->id, weight);
-							Edge* e1 = new Edge(a, b, weight);
-							count=0;
-					    	break;
-			     	}
-			    	pch = strtok (NULL, " ");
+					getline(openfile, datain);
+					datain = trim(datain);
+					//Location tag
+					if (!datain.compare("<location>"))
+					{
+						getline(openfile, datain);
+						datain = trim(datain);
+						strcpy( cdata, datain.c_str() );
+
+						mydata=0;
+						r=4;
+						while (1)
+						{
+							if (cdata[r] == '<') break;
+							mdata[mydata] = cdata[r];
+							mydata++;
+							r++;
+						}
+						mdata[mydata]='\0';
+
+						string from(mdata);
+						mnode = node_map.find_in_nodemap(from);
+
+                        getline(openfile, datain);
+                        datain = trim(datain);
+                        strcpy( cdata, datain.c_str() );
+
+                        mydata=0;
+                        r=5;
+                        while (1)
+                        {
+                            if (cdata[r] == '<') break;
+                            mdata[mydata] = cdata[r];
+                            mydata++;
+                            r++;
+                        }
+                        mdata[mydata]='\0';
+
+						mnode->lat=atof(mdata);
+
+                        getline(openfile, datain);
+                        datain = trim(datain);
+                        strcpy( cdata, datain.c_str() );
+
+                        mydata=0;
+                        r=5;
+                        while (1)
+                        {
+                            if (cdata[r] == '<') break;
+                            mdata[mydata] = cdata[r];
+                            mydata++;
+                            r++;
+                        }
+                        mdata[mydata]='\0';
+
+                        mnode->lng=atof(mdata);
+						
+						continue;
+					}
+					else if (!datain.compare("</location>"))
+					{
+						continue;
+					}
+
+					break;
+				}
+				
+				for (;;)
+				{
+					link=0;
+					strcpy( cdata, datain.c_str() );
+
+					while (cdata[link] != '>') link++;
+					link++;
+
+					mydata=0;
+					while (cdata[link] != '<')
+					{
+						if (cdata[link] == ' ')
+						{
+							mdata[mydata]='\0';
+							string id(mdata);
+							src = node_map.find_in_nodemap(id);
+							mydata=0;
+							link++;
+							continue;
+						}
+						
+						mdata[mydata] = cdata[link];
+						mydata++;
+						link++;
+					}
+					mdata[mydata]='\0';
+					string id(mdata);
+					dst = node_map.find_in_nodemap(id);
+
+					double weight = GetDistance(src->lat, dst->lng, 
+							dst->lat, dst->lng);
+					
+					#ifdef DEBUG
+						cout << "weight: " <<  weight << endl;
+					#endif
+
+					connector = new Edge(weight, dst);
+					src->neighbors.push_back(connector);
+					
+					getline(openfile, datain);
+					if (!datain.compare("</geometry>")) break;
 				}
 			}
 		}
 
-		cin >> dstart;
-		if (!strcmp(dstart, "-1"))
-				break;
+		#ifdef DEBUG
+			cout << node_map;
+		#endif
 
-		cin >> dend;
+		while (1)
+		{
+			cin >> fsrc;
+			if (!strcmp(fsrc.c_str(), "-1"))
+				exit(1);
+			cin >> fdst;
+			dijkstra(fsrc, fdst, node_map);
+			cout << node_map;
+		}
 
-		//Cal ShortPath
-		a = findnode(dstart);
-		b = findnode(dend);
-		a->dStart = 0.0; // set start node
-		Das();
-		PrintRoute(b);
-	}
-
-	for (int i=0; i<paths.size(); i++)
-	{
-		Path *sp = paths[i];
-		sp->print();
 	}
 
 	return 0;
