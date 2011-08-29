c8935667273e30613ecfdc184006e516416fc365
shulong <shulong@shulong-laptop.(none)>
2011-08-29 17:00:23 +0800

    CHG: ok

--- simple.cpp
+++ simple.cpp
@@ -3,13 +3,12 @@
 #include<fstream>
 #include<cmath>
 #include<string>
+#include<list>
+#include<map>
 #include<cstring>
+#include<queue>
+#include<cstdlib>
 #include<climits>
-//#include<stdio.h>
-#include<stdlib.h>
-
-//#define DOUBLE_MAX  99999999999
-//#define DEBUG
 
 using namespace std;
 
@@ -17,23 +16,20 @@ class Node;
 class Edge;
 class Path;
 
-const double PI = 4.0*atan(1.0);
+//#define DEBUG
+#define PI 3.14159265358979323846
 
-vector<Node*> nodes;
-vector<Edge*> edges;
-vector<Path*> paths;
+vector<Path*> mp;
 
 class Node
 {
+
 public:
-	Node(char* mid)
+	Node(string mname)
 	{
-		strcpy(id, mid);
-		previous = NULL;
-		dStart = INT_MAX;
-		nodes.push_back(this);
-		lat=0;
-		lng=0;
+		name = mname;
+		visited = false;
+		weight = lat = lng = 0.0;
 	}
 
 	void setLat(double mlat)
@@ -57,245 +53,209 @@ public:
 	}
 	
 public:
-	char id[255];
-	class Node* previous;
-	double lat;
-	double lng;
-	double dStart;
+	string    name;
+	bool      visited;
+	double       weight;
+	double		 lat;
+	double		 lng;
+	class Node      *back_pointer;
+	list<class Edge*> neighbors;
 };
 
 class Edge
 {
 public:
-	Edge(Node* node1, Node* node2, double distance)
-		: node1(node1), node2(node2), distance(distance)
+	class Node * mdest;
+	double eweight;
+
+	Edge(double c,Node *md = NULL)
 	{
-		edges.push_back(this);
+		eweight = c;
+		mdest = md;
+	}
+	~Edge(){
+		if(mdest)  delete mdest;
 	}
 
-	bool Connects(Node* node1, Node* node2)
-	{
-		return ((node1 == this->node1 && node2 == this->node2));
-	}
-public:
-	Node* node1;
-	Node* node2;
-	double distance;
+
 };
+
+map<string,class Node*> nodes;
 
 class Path
 {
 public:
-	  Path(double th, int rep)
-	  {
-		if (rep == -1)
-		 total = -1;
-		else
-		 total = th;
-		 //path.push_back(this);
-	  }
 
-	  void setP(string str)
-	  {
-			mpath.push_back(str);
-	  }
+	void setweight(double thr)
+	{
+		total = thr;
+	}
 
-	  void pushb()
-	  {
-		 paths.push_back(this);
-	  }
 
-	  void print()
-	  {
-			if (mpath.size()==1)
+	void setP(string str)
+	{
+		mpath.push_back(str);
+	}
+
+	void pushb()
+	{
+		mp.push_back(this);
+	}
+
+	void print()
+	{
+		if (mpath.size()==1)
+		{
+			cout << -1 << endl;
+			return;
+		}
+
+		cout << total << " ";
+		if (total != -1)
+		{
+			for (int i=mpath.size()-1; i>=0; i--)
 			{
-				cout << -1 << endl;
-				return;
+				cout << mpath[i] << " ";
 			}
-			cout << total << " ";
-			if (total != -1)
-			{
-				for (int i=mpath.size()-1; i>=0; i--)
-				{
-					cout << mpath[i] << " ";
-				}
-			}
-			cout << endl;
-      }
+		}
+		cout << endl;
+	}
 
-private:
-	 double total;
-	 vector<string> mpath;
+	private:
+		double total;
+		vector<string> mpath;
+};
+
+struct ca
+{  
+   bool operator()(class Node * &a, class Node * &b)const{
+      return b->weight < a->weight;      
+   }
 };
 
 
-Node* Et(vector<Node*>& nodes)
+Node* findnode(string name)
 {
-	int size = nodes.size();
-	if (size == 0) return NULL;
-
-	int smallestPosition = 0;
-	Node* smallest = nodes.at(0);
-	for (int i=1; i<size; ++i)
+	Node* cc = nodes[name];
+	if ( cc == 0 )
 	{
-		Node* current = nodes.at(i);
-		if (current->dStart < smallest->dStart)
-		{
-			smallest = current;
-			smallestPosition = i;
-		}
+		nodes[name] = new Node(name);
+		cc = nodes[name];
 	}
-	nodes.erase(nodes.begin() + smallestPosition);
-	return smallest;
+
+	return cc;
 }
 
-bool Contains(vector<Node*>& nodes, class Node* node)
+void rnodes()
 {
-	const int size = nodes.size();
-	for(int i=0; i<size; ++i)
-	{
-		if (node == nodes.at(i))
-		{
-			return true;
-		}
+	map<string,class Node*>::iterator count;
+	for(count = nodes.begin(); count != nodes.end(); count++){
+		(*count).second->weight = 0; 
+		(*count).second->back_pointer = 0;
+		(*count).second->visited = false;
 	}
-	return false;
 }
 
-vector<Node*>* AdjacentNodes(Node* node)
+void dan(string s,string t)
 {
-	vector<Node*>* adjacentNodes = new vector<Node*>();
-	const int size = edges.size();
-	for(int i=0; i<size; ++i)
-	{
-		Edge* edge = edges.at(i);
-		Node* adjacent = NULL;
-		if (edge->node1 == node)
-		{
-			adjacent = edge->node2;
+	int flag=0;	
+	Node* source = nodes[s];
+	Node* target = nodes[t];
+
+	rnodes();
+
+	priority_queue<Node *, deque<Node*>, ca> queue_r;
+	queue_r.push(source);
+	while (!queue_r.empty()){
+
+		Node* curr = queue_r.top(); 
+		queue_r.pop();
+		curr->visited = true;
+
+		list<Edge*>::iterator edge;
+		for(edge = curr->neighbors.begin(); edge != curr->neighbors.end(); edge++){
+			Node *ne = (*edge)->mdest;
+			if(!ne->visited){
+				ne->weight += (*edge)->eweight + curr->weight;
+				ne->visited = true;
+				ne->back_pointer = curr;
+				queue_r.push(ne);
+			}
+			else{
+			} 
 		}
-		else if (edge->node2 == node)
-		{
-			adjacent = edge->node1;
-		}
-		if (adjacent && Contains(nodes, adjacent))
-		{
-			adjacentNodes->push_back(adjacent);
-		}
+
 	}
-	return adjacentNodes;
-}
 
-void PrintRoute(Node* dest, int rep)
-{
-	Node* p = dest;
-	Path* path = NULL;
-
-	if (dest == NULL)
+	Path *pathr = new Path();
+	Node *ner = target;		
+	if (ner->back_pointer==NULL)
 	{
+		pathr->setweight(-1);
+		mp.push_back(pathr);
 		return;
 	}
-	else
+
+	while (ner->back_pointer!=NULL)
 	{
-		path = new Path(dest->dStart, rep);
+		//cout << "id:" << ne->name << "," << ne->weight << endl;
+		pathr->setP(ner->name);
+		if (!flag)  pathr->setweight(ner->weight),flag=1;
+		ner = ner->back_pointer;
 	}
 
-	if (dest->dStart != -1)
-	{
-	  if (rep != -1)
-	  {
-		while (p)
-		{
-		//cout << previous->id << " ";
-			path->setP(p->id);
-			p = p->previous;
-		}
-           }
-	}
-	//cout << endl;
-	path->pushb();
-}
-
-double Distance(Node* node1, Node* node2)
-{
-	const int size = edges.size();
-	for(int i=0; i<size; ++i)
-	{
-		Edge* edge = edges.at(i);
-		if (edge->Connects(node1, node2))
-		{
-			return edge->distance;
-		}
-	}
-	return -1; // should never happen
-}
-
-int Das()
-{
-	while (nodes.size() > 0)
-	{
-		Node* smallest = Et(nodes);
-		vector<Node*>* adjacentNodes =
-			AdjacentNodes(smallest);
-
-		const int size = adjacentNodes->size();
-		for (int i=0; i<size; ++i)
-		{
-			Node* adjacent = adjacentNodes->at(i);
-			double ds = Distance(smallest, adjacent);
-			if (ds == -1) 
-			{
-				//unlink
-				return -1;
-			}
-			double distance =  ds + smallest->dStart;
-			if (distance < adjacent->dStart)
-			{
-				adjacent->dStart = distance;
-				adjacent->previous = smallest;
-			}
-		}
-		delete adjacentNodes;
-	}
-
-	return 1;
-}
-
-class Node* findnode(char *p)
-{
-    const int size = nodes.size();
-    for(int i=0; i<size; ++i)
-    {	
-        if (!strcmp(p, nodes.at(i)->id))
-        {
-		 	//cout << "match: " << nodes.at(i)->id << " = " << p << endl;
-			return nodes.at(i);
-        }
-    }
-
-	return NULL;
+	pathr->setP(ner->name);
+	mp.push_back(pathr);
+	//cout << "id:" << source->name << "," << source->weight << endl;
+	
 }
 
 double ConvertDegreeToRadians(double degrees)
 {
-	return (PI/180) * degrees;
+	return (degrees/180.0)*PI;
+}
+
+double ConvertRadiansToDegree(double radians)
+{
+	return (180.0/PI) * radians;
 }
 
 double GetDistance(double Lat1, double Long1, double Lat2, double Long2)
 {
+#if 0
 	double Lat1r = ConvertDegreeToRadians(Lat1);
 	double Lat2r = ConvertDegreeToRadians(Lat2);
 	double Long1r = ConvertDegreeToRadians(Long1);
 	double Long2r = ConvertDegreeToRadians(Long2);
 
 	cout << Lat1 << "," << Lat2 << "," << Long1 << "," << Long2 << endl;
+	cout << Lat1r << "," << Lat2r << "," << Long1r << "," << Long2r << endl;
 
 	double R = 6371;
 	double d = acos(sin(Lat1r) * sin(Lat2r) + 
-			        cos(Lat1r) * cos(Lat2r) * cos(Long2r-Long1r))* R;
-	return d;
-}
+			        cos(Lat1r) * cos(Lat2r) * cos(Long2-Long1))* R;
 
+	return d;
+#endif
+
+#if 1
+	double dLat = ConvertDegreeToRadians(Lat2 - Lat1);
+	double dLon = ConvertDegreeToRadians(Long2 - Long1);
+
+	double lat1r = ConvertDegreeToRadians(Lat1);
+	double lat2r = ConvertDegreeToRadians(Lat2);
+	double long1r = ConvertDegreeToRadians(Long1);
+	double long2r = ConvertDegreeToRadians(Long2);
+
+	double a = sin(dLat/2) * sin(dLat/2) +
+        	    cos(lat1r) * cos(lat2r) * sin(dLon/2) * sin(dLon/2); 
+	double R = 6367;
+	double c = 2 * atan2(sqrt(a), sqrt(1-a)); 
+	double d = R * c;
+
+	return round(d);
+#endif
+}
 
 string LTrim(const string& str)
 {
@@ -316,33 +276,32 @@ int main(int argc, char **argv)
 {
 	string sp;
 	char file_path[255]="";
-	class Node* a = NULL;
-    	class Node* b = NULL;
+	class Node *rnode;
+	class Node *srcnode, *dstnode;
+	class Edge *medge;
+
 	int weight=0;
 	char * pch;
 	char buffer[255]="";
 	char data[255]="";
 	int count=0;
 	int loop=0;
-	char dstart[255]="",dend[255]="";
-
+	string dstart,dend;
+	int format=0;
     	cin >> file_path;
 
-	while (1)
-	{
-		nodes.clear();
-		edges.clear();
-		//FILE *file = fopen(file_path, "r");
-		if (!strcmp(file_path,"-1")) break;
-		
-		ifstream fin(file_path);
-		//FILE *file = fopen(file_path, "r");
+	loop=1;
+	//FILE *file = fopen(file_path, "r");
+	if (!strcmp(file_path,"-1")) exit(1);
+	
+	ifstream fin(file_path);
+	//FILE *file = fopen(file_path, "r");
 
-		if(!fin)
-		{    
-			cout << file_path << "file error" << endl;   
-			break;
-		}
+	if(!fin)
+	{    
+		cout << file_path << "file error" << endl;   
+		exit(1);
+	}
 
 	    while (!fin.eof())
 	    {
@@ -372,19 +331,35 @@ int main(int argc, char **argv)
 							data[ct] = buffer[j];
 						data[ct]='\0';
 
-						a = findnode(data);
-						if (a == NULL)  a = new Node(data);
+						string ko(data);
+						rnode = findnode(ko);
 						
 						//fetch lat
 						getline(fin, sp);
 						sp = trim(sp);
 						strcpy( buffer, sp.c_str() );
 						ct=0;
+
+						char cdata[255]="";
+						int inputlng=0;
+						for (int j=0; buffer[j] != '>' ;j++,ct++)
+							cdata[ct] = buffer[j];
+						cdata[ct]='\0';
+						ct=0;
 						for (int j=5; buffer[j] != '<' ;j++,ct++)
 							data[ct] = buffer[j];
 						data[ct]='\0';
 
-						a->setLat(atof(data));
+						if (!strcmp(cdata, "<lat"))
+						{
+							inputlng=1;
+							rnode->setLat(atof(data));
+						}
+						else
+						{
+							inputlng=0;
+							rnode->setLng(atof(data));
+						}
 
 						//fetch lng
 						getline(fin, sp);
@@ -394,7 +369,15 @@ int main(int argc, char **argv)
 						for (int j=5; buffer[j] != '<' ;j++,ct++)
 							data[ct] = buffer[j];
 						data[ct]='\0';
-						a->setLng(atof(data));
+
+						if (inputlng == 0)
+						{
+							rnode->setLat(atof(data));
+						}
+						else
+						{
+							rnode->setLng(atof(data));
+						}
 						
 						continue;
 					}
@@ -420,7 +403,10 @@ int main(int argc, char **argv)
 						if (buffer[j] == ' ')
 						{
 							data[ct]='\0';
-					        	a = findnode(data);
+							string ok(data);
+							srcnode = findnode(ok);
+
+					        	//a = findnode(data);
 							ct=0;
 							continue;
 						}
@@ -429,75 +415,72 @@ int main(int argc, char **argv)
 						ct++;
 					}
 					data[ct]='\0';
-				       b = findnode(data);
+					string ok(data);
+					dstnode = findnode(ok);
 					#ifdef DEBUG
-						cout << a->id << " -> " << b->id << endl;
-						cout << a->getLat() << "," << a->getLng() << endl;
-						cout << b->getLat() << "," << b->getLng() << endl;
+						cout << srcnode->name << " -> " << dstnode->name << endl;
+						cout << srcnode->getLat() << "," << srcnode->getLng() << endl;
+						cout << dstnode->getLat() << "," << dstnode->getLng() << endl;
 					#endif
 
-					double weight = GetDistance(a->getLat(), a->getLng(), 
-							b->getLat(), b->getLng());
+					double weight = GetDistance(srcnode->getLat(), srcnode->getLng(), dstnode->getLat(), dstnode->getLng());
 					
 					#ifdef DEBUG
 						cout << weight << endl;
 					#endif
 
-					Edge* e1 = new Edge(a, b, weight);
+					medge = new Edge(weight, dstnode);
+					srcnode->neighbors.push_back(medge);
 
 					getline(fin, sp);
-					if (!sp.compare("</geometry>\r")) break;
+					sp = trim(sp);
+					if (!sp.compare("</geometry>")) break;
 				}
 			}
 			else
 			{
-	  			pch = strtok (buffer," ");			
-
-				while (pch != NULL)
-				{
-			    	switch (count)
-		        	{
-						case 0:
-							a = findnode(pch);
-							if (a == NULL)  a = new Node(pch);
-			    				count++;
-							break;
-						case 1:
-							b = findnode(pch);
-							if (b == NULL)  b = new Node(pch);
-			    				count++;
-							break;
-						case 2:
-							weight= atoi(pch);
-							//printf("%s -> %s, w=%d\n", a->id, b->id, weight);
-							Edge* e1 = new Edge(a, b, weight);
-							count=0;
-					    	break;
-			     	}
-			    	pch = strtok (NULL, " ");
-				}
+				format=1;
 			}
 		}
 
-		cin >> dstart;
-		if (!strcmp(dstart, "-1"))
-				break;
+		if (format == 1)
+		{
+			ifstream other(file_path);
+			//FILE *file = fopen(file_path, "r");
 
-		cin >> dend;
+			if(!other)
+			{    
+				cout << file_path << "file error" << endl;   
+				exit(1);
+			}
+			string aid,bid;
 
-		//Cal ShortPath
-		a = findnode(dstart);
-		b = findnode(dend);
-		a->dStart = 0.0; // set start node
-		int rep = Das();
-		PrintRoute(b, rep);
-	}
+			while (other >> aid >> bid >> weight)
+			{
+				srcnode = findnode(aid);
+				dstnode = findnode(bid);
+				medge = new Edge(weight, dstnode);
+				srcnode->neighbors.push_back(medge);
+				
+			} 
+		}
 
-	for (int i=0; i<paths.size(); i++)
-	{
-		Path *sp = paths[i];
-		sp->print();
-	}
+		while (1)
+		{
+			cin >> dstart;
+			if (dstart == "-1")
+					break;
+
+			cin >> dend;
+			dan(dstart, dend);
+			//cout << node_map;
+		}
+		for (int i=0; i<mp.size(); i++)
+		{
+			Path *mpr = mp[i];
+			mpr->print();
+		}
+
 
 	return 0;
 }
